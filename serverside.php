9dab09012a8d45053d6986630a26cf85f1ab4998
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2019-03-20 12:23:58 +0100

    Some php-output

--- serverside.php
+++ serverside.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test";
+$dbname = "jokes";
 if (isset($_GET['sample'])) {
 	sampleJoke();
 } else {
@@ -12,14 +12,37 @@ function sampleJoke()
 {
 	echo "Hello i skogen";
 }
+
 // PHP JOT
 try {
 	$myPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$myPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "CREATE DATABASE fiskmacka";
-	$myPDO->exec($sql);
-	echo "Det funkar hurra";
+	//$sql = "CREATE DATABASE fiskmacka";
+	//$myPDO->exec($sql);
+	//echo "Det funkar hurra";
 
+    // sql to create table
+//	$sql = "CREATE TABLE MyGuests (
+//	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+//	firstname VARCHAR(30) NOT NULL,
+//	lastname VARCHAR(30) NOT NULL,
+//	email VARCHAR(50),
+//	reg_date TIMESTAMP
+//)";
+
+// use exec() because no results are returned
+//$myPDO->exec($sql);
+$sql = $myPDO->prepare("SELECT id, joke FROM jokes");
+$sql->execute();
+
+$result = $sql->fetchAll();
+print_r($result);
+
+
+// Utnyttja svaret
+
+
+//echo "Table MyGuests created successfully";
 } catch (PDOException $e) {
 	echo 'Connection failed: ' . $e->getMessage();		
 }
