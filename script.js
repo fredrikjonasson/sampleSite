5ef1dcd9cfa528820d7b4d31c61671ea1d0fdcd9
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2019-03-27 22:06:53 +0100

    Sampling

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.onload = function () {
 	document.getElementById("sample").addEventListener("click", jokeSampler);
-	var departureContainer = document.getElementById("laughHolder");
+	//var departureContainer = document.getElementById("laughHolder");
 }
 function jokeSampler() {
 	const request = new XMLHttpRequest();
 	request.open('GET', 'http://localhost:80/serverside.php', true);
 	request.onreadystatechange = function () {
-		if(request.readyState === 4 && request.status === 200) {
-			console.log(request.responseText);
+		if (request.readyState === 4 && request.status === 200) {
+			//console.log(request.responseText);
+			document.getElementById("laughHolder").innerHTML = request.responseText;
 		}
 	}
 	request.send();
-    //var htmlString = " "
-    //htmlString += "<p>Insertjoke here</p>"
-    //laughHolder.insertAdjacentHTML('beforeend', htmlString)
+	//var htmlString = " "
+	//htmlString += "<p>Insertjoke here</p>"
+	//laughHolder.insertAdjacentHTML('beforeend', htmlString)
 }
--- serverside.php
+++ serverside.php
@@ -1,18 +1,38 @@
 <?php
+//echo phpinfo();
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "jokes";
-if (isset($_GET['sample'])) {
-	sampleJoke();
-} else {
-	echo "Fail Fajl";
-}
-function sampleJoke()
-{
-	echo "Hello i skogen";
-}
+// if (isset($_GET['sample'])) {
+// 	sampleJoke();
+// } else {
+// 	echo "Fail Fajl";
+// }
+// function sampleJoke()
+// {
+// 	echo "Hello i skogen";
+// }
+echo "<table style='border: solid 1px black;'>";
+//echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
 // PHP JOT
 try {
 	$myPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -21,33 +41,45 @@ try {
 	//$myPDO->exec($sql);
 	//echo "Det funkar hurra";
 
-    // sql to create table
-//	$sql = "CREATE TABLE MyGuests (
-//	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-//	firstname VARCHAR(30) NOT NULL,
-//	lastname VARCHAR(30) NOT NULL,
-//	email VARCHAR(50),
-//	reg_date TIMESTAMP
-//)";
-
-// use exec() because no results are returned
-//$myPDO->exec($sql);
-$sql = $myPDO->prepare("SELECT id, joke FROM jokes");
-$sql->execute();
+	// sql to create table
+	//	$sql = "CREATE TABLE MyGuests (
+	//	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	//	firstname VARCHAR(30) NOT NULL,
+	//	lastname VARCHAR(30) NOT NULL,
+	//	email VARCHAR(50),
+	//	reg_date TIMESTAMP
+	//)";
 
 
-//Vilken är vettig att ha till höger om ->
-$result = $sql->fetchAll(PDO::FETCH_COLUMN, 1);
+	// use exec() because no results are returned
+	//$myPDO->exec($sql);
 
-print_r($result);
+	// Get the number of database entries.
+	$entries = $myPDO->prepare("SELECT COUNT(*) FROM jokes");
+	$entries->execute();
+	// Sample one of these numbers.
+	$numEntries = random_int(1, (int) $entries->fetchColumn());
+
+	// Sample a joke from the database.
+	$sql = $myPDO->prepare("SELECT id, joke FROM jokes WHERE id=$numEntries");
+	$sql->execute();
+
+	// Skriv ut skämtet
+	$result = $sql->setFetchMode(PDO::FETCH_ASSOC);
+	foreach (new TableRows(new RecursiveArrayIterator($sql->fetchAll())) as $k => $v) {
+		echo $v;
+	}
+
+	print_r($result);
 
 
-// Utnyttja svaret
-
-
-//echo "Table MyGuests created successfully";
+	// Utnyttja svaret
+	$myPDO = NULL;
+	$entries = NULL;
+	$sql = NULL;
+	
+	//echo "Table MyGuests created successfully";
 } catch (PDOException $e) {
-	echo 'Connection failed: ' . $e->getMessage();		
+	echo 'Connection failed: ' . $e->getMessage();
 }
-
-?>	
+ 
